b519d21708a1947d57461810c3e0c8aec5c2e528
tuboss4u <league@WTS10.attlocal.net>
2016-02-19 15:00:21 -0800

    Day 5
    Day 5

--- src/day3/DialogAttacks.java
+++ src/day3/DialogAttacks.java
@@ -4,12 +4,14 @@ import javax.swing.JOptionPane;
 
 public class DialogAttacks {
 public static void main(String[] args){
-	String name = JOptionPane.showInputDialog("Hello and welcome to the DialogAttacks® Machine. \n Question 1: What is your name? \n (Please don't answer in complete sentences.)");
+	String name = JOptionPane.showInputDialog("Hello and welcome to the DialogAttacks® Machine. \nQuestion 1: What is your name? \n(Please don't answer in complete sentences.)");
 	JOptionPane.showMessageDialog(null, "Hello " + name + "!");
-	String bday = JOptionPane.showInputDialog("When is your birthday? \n Please enter in MM/DD/YYYY form.");
+	String bday = JOptionPane.showInputDialog("When is your birthday? \nPlease enter in MM/DD/YYYY form.");
 	JOptionPane.showMessageDialog(null, "Confirming that your birthday is " + bday + ".");
-	String email = JOptionPane.showInputDialog("If I were to need to contact you, what is your email? \n Please answer in form of name@example.com.");
-	JOptionPane.showMessageDialog(null, "If something goes wrong, you will recieve an email at" + email + ". No worries, though.");
-	String adress = JOptionPane.showInputDialog("Where is your adress? \n Answer like this: Street, City, State, ZIPCODE.");
+	String email = JOptionPane.showInputDialog("If I were to need to contact you, what is your email? \nPlease answer in form of name@example.com.");
+	JOptionPane.showMessageDialog(null, "If something goes wrong, you will recieve an email at " + email + ". No worries, though.");
+	String adress = JOptionPane.showInputDialog("Where is your adress? \nAnswer like this: Number, Street, City, State, ZIPCODE.");
+	JOptionPane.showMessageDialog(null,"You are located at " + adress + ".");
+	JOptionPane.showMessageDialog(null, "Thank you for sacraficing your personal information so people can use your personal information to hack into the IRS and steal your tax refunds. \nHave a good day! \nCopyright Evil Hackers, Inc., 2016.");
 }
 }
--- src/day5/FlamingNinjaStar.java
+++ src/day5/FlamingNinjaStar.java
@@ -11,34 +11,40 @@ public class FlamingNinjaStar {
 
 		int baseSize = 300;		//the size of the black part of the star
 		int flameSize = 200;		//the length of the flaming arms
-
+		Robot ninja = new Robot("mini");
 		// *14. Use the methods setX and setY to move the ninja star into the center of the screen
-		
+		ninja.setX(900);
+		ninja.setY(500);
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 1. Make a new robot, and set it's pen down.
 
 		// 12. Set the robot speed to 10
-
+ninja.setSpeed(10);
 		// 13. Make all the code below repeat 25 times
+for (int i = 0; i < 25; i++) {
 
 			// 2. Turn the robot 1/8 of a circle
-		
+		ninja.penDown();
+		ninja.turn(45);
 			// 3. Move the robot 64 pixels
-
+		ninja.move(64);
 			
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+		ninja.turn(-40);
 			// 5. Move the robot the distance in the variable flameSize
-			
+		ninja.setPenColor(255, 255, 34);
+		ninja.move(200);
 			// 6. Turn the robot 170 degrees
-			
+		ninja.turn(170);
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+		ninja.move(flameSize);
 			// 8. Turn the robot 64 degrees to the right
-			
+		ninja.setPenColor(0,0,0);
+		ninja.turn(64);
 			// 9. Move the robot the distance in the variable baseSize
-			
+		ninja.move(baseSize);
+}
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
 			// 11. Color your ninja star like Figure 2.
 		
