6c3926c1a5bf2f2a307c9878893debce5b7e4bcb
Love4653 <league@WTS10.attlocal.net>
2016-03-05 09:58:55 -0800

    Level 0 Class 1

--- src/day3/PopUp.java
+++ src/day3/PopUp.java
@@ -3,7 +3,17 @@ package day3;
 import javax.swing.JOptionPane;
 
 public class PopUp {
-public static void main (String [] args){
-	JOptionPane.showMessageDialog(null, "Good Afternoon!");
-}
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Good Afternoon!");
+		JOptionPane.showMessageDialog(null, "This is a text speaker.");
+		for (int i = 0; i < 10; i++) {
+			String speak = JOptionPane.showInputDialog("Say something the computer will speak.");
+			speak(speak);
+		}
+	}
+
+	private static void speak(String speak) {
+		// TODO Auto-generated method stub
+
+	}
 }
